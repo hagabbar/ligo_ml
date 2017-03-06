ec99a3c8485a51ada26a54b2a220e2d78efa3355
Hunter Arthur Gabbard <hunter.gabbard@ldas-pcdev2.ligo-wa.caltech.edu>
2017-03-06 08:49:46 -0800

    Fixed multi file bug

--- timeseries_features/digest_timeseries_data.py
+++ timeseries_features/digest_timeseries_data.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 import sys
+import argparse
+
 from numpy import *
 from numpy.fft import fft, ifft, rfft, irfft
 import scipy.signal as sig
@@ -37,24 +39,38 @@ def chunk_segments(seg_lst, chunk_size, pad_size):
                 yield (t1+tt,t1+duration)
 
 if __name__ == "__main__":
-    ifo=sys.argv[1]
-    st=int(sys.argv[2])
-    et=int(sys.argv[3])
-    chan_file=sys.argv[4]
+
+    parser = argparse.ArgumentParser(description='Generate target features for machine learning on LIGO data.')
+    parser.add_argument('--ifo',type=str,required=True)
+    parser.add_argument('-f','--segment-file',type=str)
+    parser.add_argument('-s','--start-time',type=int)
+    parser.add_argument('-e','--end-time',type=int)
+    parser.add_argument('-c','--channels',type=str,required=True)
+
+    args = parser.parse_args()
+
+    ifo=args.ifo
+
+    if args.segment_file:
+        sci_segs=DataQualityFlag.read(args.segment_file)
+        assert sci_segs.ifo == ifo
+        segs=sci_segs.active
+    elif args.start_time and args.end_time:
+        segs=[Segment(args.start_time, args.end_time)]
+    else:
+        print "Either --segment-file, or both start and end time must be provided."
+        exit(2)
+
+    st=segs[0].start
+    et=segs[-1].end
+
+    chan_lst=load_channels(args.channels)
 
     chunk=16384
     pad=256
 
-    target_chan=ifo+':GDS-CALIB_STRAIN'
-
-    chan_lst=load_channels(chan_file)
-
-    segs=get_science_segments(ifo,st,et)
-    for seg in segs:
-        print seg.start, seg.end
-
-
     full_data=[]
+    subsys_new=''
     for idx,chan in enumerate(chan_lst):
         print 'Getting data for', chan
         if idx == 0:
@@ -74,11 +90,13 @@ if __name__ == "__main__":
             full_data.append(concatenate(position_chunks))
             full_data.append(concatenate(velocity_chunks))
             subsys_orig = chan.split(':')[1].split('_')[0]
-        elif subsys_new =! subsys_orig:
+        elif subsys_new != subsys_orig:
             # Turn into a big array and dump to a file
             full_data=array(full_data)
             save("%s-%s-ML-%u-%u.npy"%(ifo,subsys_orig,st,et-st),full_data)
             full_data = []
+            position_chunks=[]
+            velocity_chunks=[]
             for t1,t2 in chunk_segments(segs,chunk,pad):
                 print 'Getting chunk', t1, t2
                 data=TimeSeries.find(chan,t1,t2,ifo+'_R',nproc=20,verbose=True)
