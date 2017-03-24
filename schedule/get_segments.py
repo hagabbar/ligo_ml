c4dbee49ea9c4d40d59c7f845e82adaca093729b
Hunter Arthur Gabbard <hunter.gabbard@ldas-pcdev2.ligo-wa.caltech.edu>
2017-03-24 08:40:07 -0700

    segments are now 3600s or less

--- schedule/get_segments.py
+++ schedule/get_segments.py
@@ -9,16 +9,47 @@ def get_science_segments(ifo, st, et, min_length=1800):
     
     Returns: gwpy.segments.DataQualityFlag""" 
     scimode=DataQualityFlag.query(ifo+':DMT-ANALYSIS_READY:1',st,et)
-    print scimode.known
-    print scimode.active
+    #print scimode.known
+    #print scimode.active
     bad_segs=[flg for flg in scimode.active if abs(flg) <= min_length]
     for flg in bad_segs:
         scimode.active.remove(flg)
-    # Force all segments to be even length and not greater than 20000 seconds
+
+    # Force all segments to be even length
     for idx,flg in enumerate(scimode.active):
         if not abs(flg) % 2 == 0:
-            scimode.active[idx]=type(flg)(flg.start,flg.end-1)
+            scimode.active[idx]=type(flg)(flg.start, flg.end-1)
+    return scimode 
 
+def cut_sci_segs(scimode):  
+    """Setting segment length equal to 3600s or less.
+    Returns: gwpy.segments.DataQualityFlag"""  
+    t = 0
+    end = scimode.active[-1]
+    
+   
+    for idx,flg in enumerate(scimode.active):
+        #if      
+        for idx2,seg in enumerate(range(flg.start, flg.end, 3600)):
+            if (flg.end - seg) < 3600:
+                seg_start = seg
+                seg_end = seg + (flg.end - seg) 
+                if t >= idx:
+                    scimode.active.append(type(flg)(seg_start, seg_end))
+                else:
+                    scimode.active[idx2 + t]=type(flg)(seg_start, seg_end)
+            else:
+                seg_start = seg
+                seg_end = seg + 3600
+                if t >= idx:
+                    scimode.active.append(type(flg)(seg_start, seg_end))
+                else:
+                    scimode.active[idx2 + t]=type(flg)(seg_start, seg_end)
+        t += idx2 + 1
+        if flg == end:
+            break
+    print scimode.active
+    return scimode
 
 #        if not abs(flg) % 2 == 0 and abs(flg) >= 60000:
 #            scimode.active[idx]=type(flg)(flg.start,(flg.end-1)/4)
@@ -32,7 +63,7 @@ def get_science_segments(ifo, st, et, min_length=1800):
 #            scimode.active[idx]=type(flg)(flg.start,flg.end-1)
 #    print scimode
 #    sys.exit()
-    return scimode
+    #return scimode
 
 if __name__ == "__main__":
     ifo=sys.argv[1]
@@ -40,5 +71,6 @@ if __name__ == "__main__":
     et=int(sys.argv[3])
 
     scimode = get_science_segments(ifo, st, et)
-    scimode.write('%s-SCISEGS-%u-%u.hdf'%(ifo,st,et-st))
+    scimode = cut_sci_segs(scimode)
+    scimode.write('%s-SCISEGS-%u-%u.hdf5'%(ifo,st,et-st))
 
